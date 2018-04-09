168172376a4d22e1a8a162ce646c5e303b2f3940
k5i ogura <kenji.ogura@jcom.zaq.ne.jp>
2018-04-09 13:04:35 +0900

    revise

---
+++ scripts/lcurve.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import numpy as np
+import math
+import re
+import sys
+from pdb import *
+
+with open(sys.argv[1]) as f:
+    l2 = f.readlines()
+    #print('%d lines'%len(l2))
+
+avg_min = 9999999
+min_ech = 0
+weights = min_weights = ""
+lastavg = 0
+for m in l2:
+    ech = re.search('[0-9]+',str(m))
+    avg = re.findall('([0-9]+\.[0-9]+) +avg',str(m))
+    wgt = re.findall('Saving weights to (\S+)',str(m))
+    bup = len(re.findall('.backup',str(m)))
+    if len(wgt)!=0 and bup != 1: weights = str(wgt[0])
+    if len(avg)==0: continue
+    if len(avg)==1:
+        avg = float(avg[0])
+        lastavg = avg
+        if math.isnan(avg):lastavg = 9999999.
+        epoch = int(ech.group())
+        if avg_min>avg: avg_min, min_ech, min_weights = (avg, epoch, weights)
+    #   print('%d %.5f'%(epoch, avg))
+print('MinimalLoss: %d epoch %.5f loss %s'%(min_ech, avg_min, min_weights))
+print('LastStage  : %d epoch %.5f loss %s'%(epoch, lastavg, weights))
