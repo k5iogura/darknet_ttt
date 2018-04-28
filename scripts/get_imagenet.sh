23fd2e0300e121c84b6e3c94cb9d6dd89bcd58d9
Kenji Ogura <kenji.ogura@jcom.zaq.ne.jp>
2018-04-28 15:11:18 +0900

    revise

--- scripts/get_imagenet.sh
+++ scripts/get_imagenet.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
 
-export getn=0
-export rmvn=0
+#enable job control
+set -m
 
-for i in `find lists -name \*.txt`;
-do
-    export dir=`basename $i | sed 's/\.txt//'`
+#download and cleanup sub-shell
+get_1list() {
+    export dir=`basename $1 | sed 's/\.txt//'`
+    if [ -d images/$dir ];
+    then
+        echo images/$dir already found, skipped >> images/${dir}.log
+        return
+    fi
+
     echo downloading $dir
-    wget -i $i -P images/$dir -T 1 -t 1 -nc >& log
+    wget -i $1 -P images/$dir -T 1 -t 1 -nc >& /dev/null
+    echo wget done >> images/${dir}.log
 
-echo clean up $dir
-if [ ! -d $i ];
+    if [ ! -d images/$dir ];
+    then
+        echo images/$dir not found
+        return
+    fi
+
+    echo cleaning $dir >> images/${dir}.log
+    for j in `find images/$dir -name \*.[jJ]\*`;
+    do
+        export JPEG=`file $j | grep JPEG | wc -l | awk '{print $1;}'`
+        if [ $JPEG -eq 0 ];
+        then
+            rm -f $j
+        fi
+    done
+    echo clean up $dir done >> images/${dir}.log
+}
+
+#main
+if [ $# -ne 1 ];
 then
-continue
+    echo Usage $0 lists
+    exit
 fi
-for j in `find images/$dir -name \*.[jJ]\*`;
+
+if [ ! -d $1 ];
+then
+    echo $1 directory not found
+    exit
+fi
+
+#execute sub-shell with multi-process
+for i in `find $1 -name \*.txt`;
 do
-export JPEG=`file $j | grep JPEG | wc -l | awk '{print $1;}'`
-if [ $JPEG -eq 0 ];
-then
-    rm -f $j
-    export rmvn=`expr $rmvn + 1`
-else
-    export getn=`expr $getn + 1`
-fi
+    get_1list $i &
 done
-echo getn/rmvn = $getn / $rmvn
-done
+
+#wait sub-shell exiting
+#fg
