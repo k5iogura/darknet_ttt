5c499c5d8e45722fbe86b6e31f07c2aafdf5af60
k5i ogura <kenji.ogura@jcom.zaq.ne.jp>
2018-08-09 19:01:21 +0900

    "revise"

--- src/convolutional_layer.c
+++ src/convolutional_layer.c
@@ -647,7 +647,7 @@ void forward_convolutional_layer_hf(convolutional_layer l, network net)
 #else
         error("Need OPENEXR Define-0");
 #endif
-    }else if(1){    // for gemm1_halfxf_halfx9.cl im2col+row2col_major
+    }else if(0){    // for gemm1_halfxf_halfx9.cl im2col+row2col_major
 #ifdef OPENEXR
         float *b = net.workspace;
         float *c = l.output;
@@ -689,31 +689,43 @@ void forward_convolutional_layer_hf(convolutional_layer l, network net)
         //gemm2(1,1,1, m, n, k, 1, a, m, b, k, 1, c, m);     //OK for instead of FPGA Model
         gemm2(0,1,1, m, n, k, 1, A, k, b, k, 1, c, m);     //OK for instead of FPGA Model
         free(A);
-    }else if(0){ // with FPGA Model for gemm_ntt.cl and gemm_ntt_jik.cl and gemm_ntt_jikK.cl
+    }else if(1){ // with FPGA Model for gemm_ntt.cl and gemm_ntt_jik.cl and gemm_ntt_jikK.cl
 #ifdef OPENEXR
-        float *a = net.workspace;
-        float *b = l.weights;
-        float *c = l.output;
-        float *A = (float*)malloc(sizeof(float)*(l.out_w*l.out_h)*(l.size*l.size*l.c));
-        half *a_hf = net.workspace_hf;
-        half *b_hf = l.weights_hf;
-        half *c_hf = l.output_hf;
-        TensorDim in_dim  ={ 1, l.c, l.h, l.w };
-        TensorDim filt_dim={ l.out_c, l.c, l.size, l.size };
-        CppConvnetIm2Row(a, net.input, out_w, out_h, k, in_dim, filt_dim, l.stride, l.pad);
-        //col2row_cblas(l.c*l.size*l.size, out_w*out_h, a, A);
-        col2row_major(l.c*l.size*l.size, out_w*out_h, a, A);
-        //col2row_major(k,m,b,B);
-        //row2col_major(l.c*l.size*l.size, out_w*out_h, A, a);
+        //if(net.index==0 || net.index==2){
+        if(1 && (net.index==0 || net.index==2 || net.index==7)){
+            float *a = net.workspace;
+            float *b = l.weights;
+            float *c = l.output;
+            TensorDim in_dim  ={ 1, l.c, l.h, l.w };
+            TensorDim filt_dim={ l.out_c, l.c, l.size, l.size };
+            CppConvnetIm2Row(a, net.input, out_w, out_h, k, in_dim, filt_dim, l.stride, l.pad);
+            printf("%9.6f ", what_time_is_it_now()-time);
+            cblas_sgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1, a, m, b, k, 1, c, m); //OK
+        }else{
+            float *a = net.workspace;
+            float *b = l.weights;
+            float *c = l.output;
+            float *A = (float*)malloc(sizeof(float)*(l.out_w*l.out_h)*(l.size*l.size*l.c));
+            half *a_hf = net.workspace_hf;
+            half *b_hf = l.weights_hf;
+            half *c_hf = l.output_hf;
+            TensorDim in_dim  ={ 1, l.c, l.h, l.w };
+            TensorDim filt_dim={ l.out_c, l.c, l.size, l.size };
+            CppConvnetIm2Row(a, net.input, out_w, out_h, k, in_dim, filt_dim, l.stride, l.pad);
+            //col2row_cblas(l.c*l.size*l.size, out_w*out_h, a, A);
+            col2row_major(l.c*l.size*l.size, out_w*out_h, a, A);
+            //col2row_major(k,m,b,B);
+            //row2col_major(l.c*l.size*l.size, out_w*out_h, A, a);
 
-        float2half(m*k, A, 1, a_hf, 1);
-        printf("%9.6f ", what_time_is_it_now()-time);
-        gemm_hf(0,1,1, m, n, k, 1, a_hf, k, b_hf, k, 1, c_hf, m);     //OK for instead of FPGA Model
-        half2float(m*n, c_hf, 1, c, 1);
-        free(A);
-#else
-        error("Need OPENEXR Define-1");
-#endif
+            float2half(m*k, A, 1, a_hf, 1);
+            printf("%9.6f ", what_time_is_it_now()-time);
+            gemm_hf(0,1,1, m, n, k, 1, a_hf, k, b_hf, k, 1, c_hf, m);     //OK for instead of FPGA Model
+            half2float(m*n, c_hf, 1, c, 1);
+            free(A);
+    #else
+            error("Need OPENEXR Define-1");
+    #endif
+        }
     }
 
     if(!l.batch_normalize){
